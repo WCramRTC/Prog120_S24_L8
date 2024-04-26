ca80ea2724b9733d57f115da8b13113bb7fdb5f0
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-04-25 17:36:30 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Prog120_S24_L8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+
+
+        } // Main
+
+    } // class
+
+} // namespace
